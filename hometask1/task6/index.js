fe0cd5ae09e36f1b4ca28831adf18c701d55a488
akopyanmaxim577 <akopyanmaxim57@gmail.com>
2022-05-19 18:28:24 +0000

    Create and do 6 task

---
+++ hometask1/task6/index.js
@@ -0,0 +1,9 @@
+const nam = "Max";
+const surname = "Akopyan";
+const group = 2;
+const date = 2003;
+const married = false;
+console.log(typeof(group),group,typeof(date),date,typeof(married),married,typeof(nam),nam,typeof(surname),surname);
+const first = undefined;
+const second = null;
+console.log(typeof(first),typeof(second));

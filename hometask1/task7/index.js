4686263cdef62b2706867e2a2e5514e71d604755
akopyanmaxim577 <akopyanmaxim57@gmail.com>
2022-05-19 18:46:25 +0000

    Create and do 7 task

---
+++ hometask1/task7/index.js
@@ -0,0 +1,4 @@
+let admin = prompt("Введіть логін");
+let gmail = prompt("Введіть пошту");
+let password = prompt("Введіть пароль");
+alert(`Dear ${admin}, your email is ${gmail},, your password is ${password}`);
